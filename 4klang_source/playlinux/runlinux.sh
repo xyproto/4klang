72acca8986c865cd9e36eb71ba0993b903f3269c
Alexander F. Rødseth <rodseth@gmail.com>
2020-05-05 16:41:13 +0200

    Initial files for building 4klang on 64-bit Linux

---
+++ 4klang_source/playlinux/runlinux.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+echo "Copying 4klang.asm and 4klang.inc from parent dir"
+echo "Enabling SINGLE_TICK_RENDERING (and AUTHORING) flag for rendering example"
+echo "You need sox installed to get audio output"
+sed -e 's/\;\%define SINGLE_TICK_RENDERING/\%define SINGLE_TICK_RENDERING/' ../4klang.inc | \
+    sed -e 's/\;\%define AUTHORING/\%define AUTHORING/' > 4klang.inc
+cp ../4klang.asm .
+#yasm -f elf32 4klang.asm
+yasm -f elf64 4klang.asm
+#gcc -Wl,-no_pie -m32 4klang.o 4klangrender.c -o 4klangrender
+gcc -Wl,-no-pie -Os 4klang.o 4klangrender.c -o 4klangrender
+./4klangrender | sox -t raw -b 32 -e float -r 44100 -c 2 - -d
